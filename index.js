91bef7623fd89900dfeff56ea5ad2d4197603886
S M SADI <smsadi2001@gmail.com>
2023-12-04 10:16:45 +0600

    fourth project

---
+++ index.js
@@ -0,0 +1,71 @@
+let icon= document.querySelector("#icon");
+
+if(localStorage.getItem("theme")==null){
+    localStorage.setItem("theme","light");
+}
+
+let localData = localStorage.getItem("theme");
+
+icon.addEventListener("click", ()=>{
+
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme")){
+        icon.src ="images/sun.png";
+        localStorage.setItem("theme","dark");
+    }else{
+        icon.src="images/moon.png";
+        localStorage.setItem("theme","light");
+    }
+
+})
+
+
+
+
+
+
+
+/*
+
+
+
+let icon= document.querySelector("#icon");
+
+
+if(localStorage.getItem("theme")==null){
+    localStorage.setItem("theme","light");
+}
+
+
+let localData = localStorage.getItem("theme");
+
+
+
+if(localData=="light"){
+    icon.src="images/moon.png"
+    document.body.remove("dark-theme");
+}else if(localData=="dark"){
+    icon.src="images/sun.png"
+    document.body.add("dark-theme");
+}
+
+
+icon.addEventListener("click", ()=>{
+
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme")){
+        icon.src ="images/sun.png";
+        localStorage.setItem("theme","dark");
+    }else{
+        icon.src="images/moon.png";
+        localStorage.setItem("theme","light");
+    }
+
+})
+
+
+
+
+*/
